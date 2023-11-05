281f6a5854f30c14bfdc97daea444187d1eff4b9
Tarcisio <tarcisio.valentim@rumina.com.br>
2023-11-05 17:16:22 -0300

    first commit

---
+++ lib/dualpop_commons_dependencies.dart
@@ -0,0 +1,5 @@
+library dualpop_commons_dependencies;
+
+export 'package:flutter_mobx/flutter_mobx.dart';
+export 'package:flutter_mobx/version.dart';
+export 'package:flutter_modular/flutter_modular.dart';
